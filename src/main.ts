eb4205e44a0a9be2fa9a291ad6e3c2a6ebd31176
h7ml <h7ml@qq.com>
2023-05-16 00:41:34 +0800

    feat: 添加swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  const options = new DocumentBuilder()
+    .setTitle('Nakoruru Backend API')
+    .setDescription('The Nakoruru Backend API description by h7ml')
+    .setVersion('0.0.1')
+    .addTag('nakoruru')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
