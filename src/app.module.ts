ebd14faea688738a3fd7317cba8cbffc656ddc8e
h7ml <h7ml@qq.com>
2023-05-16 00:28:57 +0800

    feat: 展示静态页面

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { GirlModule } from './girl/girl.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigModule } from '@nestjs/config';
 
+import { join } from 'path';
 @Module({
-  imports: [GirlModule],
+  imports: [
+    ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'src/pages/home/'),
+      exclude: ['/api*'],
+    }),
+    GirlModule,
+  ],
   controllers: [],
   providers: [],
 })
