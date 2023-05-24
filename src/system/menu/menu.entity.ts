38457961ae3923a45bc1aef2258359a263a3e19b
h7ml <h7ml@qq.com>
2023-05-24 22:15:00 +0800

    feat: 使用sqlpub 免费mysql数据库

--- src/system/menu/menu.entity.ts
+++ src/system/menu/menu.entity.ts
@@ -1,21 +1,39 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  OneToMany,
+} from 'typeorm';
 import { ApiProperty } from '@nestjs/swagger';
 
+@Entity()
 export class Menu {
   @ApiProperty()
+  @PrimaryGeneratedColumn()
   id: number;
 
   @ApiProperty()
+  @Column()
   title: string;
 
   @ApiProperty()
+  @Column()
   path: string;
 
   @ApiProperty({ required: false })
+  @Column({ nullable: true })
   icon?: string;
 
   @ApiProperty({ required: false })
+  @Column({ nullable: true })
   parentId?: number;
 
   @ApiProperty({ required: false })
+  @ManyToOne(() => Menu, (menu) => menu.children)
+  parent?: Menu;
+
+  @ApiProperty({ required: false })
+  @OneToMany(() => Menu, (menu) => menu.parent)
   children?: Menu[];
 }
--- src/system/menu/menu.module.ts
+++ src/system/menu/menu.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MenuController } from './menu.controller';
 import { MenuService } from './menu.service';
+import { Menu } from './menu.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Menu])],
   controllers: [MenuController],
   providers: [MenuService],
 })
