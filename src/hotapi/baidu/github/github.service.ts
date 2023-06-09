026b8e2850885fc21ba3432809866f5ae711e15d
h7ml <h7ml@qq.com>
2023-06-09 09:07:51 +0800

    feat: 完善百度开发者github 榜单接口传值

--- src/hotapi/baidu/github/github.service.ts
+++ src/hotapi/baidu/github/github.service.ts
@@ -3,18 +3,24 @@ import axios from 'axios';
 
 @Injectable()
 export class GithubService {
-  async getGithubData() {
-    const url =
-      'https://kaifa.baidu.com/rest/v1/home/github?optionLanguage=&optionSince=';
+  async getGithubData(optionLanguage: string, optionSince: string) {
+    const url = `https://kaifa.baidu.com/rest/v1/home/github?optionLanguage=${optionLanguage}&optionSince=${optionSince}`;
 
     try {
       const response = await axios.get(url);
       // 处理响应数据
-      const githubData = response.data;
-      return githubData;
+      const githubData = response.data?.data;
+      return {
+        code: 200,
+        data: githubData,
+        message: 'success',
+      };
     } catch (error) {
-      // 处理错误
-      throw new Error('Failed to fetch GitHub data');
+      return {
+        code: 500,
+        data: [],
+        message: error,
+      };
     }
   }
 }
