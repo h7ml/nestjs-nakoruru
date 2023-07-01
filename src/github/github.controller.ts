0776adc61947bf9f13a6c3201f57e7f2b1b41bd0
h7ml <h7ml@qq.com>
2023-07-02 04:34:25 +0800

    feat: generateReadme

--- src/github/github.controller.ts
+++ src/github/github.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Header } from '@nestjs/common';
 import { ApiExcludeEndpoint, ApiTags } from '@nestjs/swagger';
 import axios from 'axios';
+const apiPrefix = 'https://nestjs.h7ml.cn/api/hotapi/';
 
 interface Article {
   id?: string | number;
@@ -94,9 +95,7 @@ export class GithubController {
 async function generateHotList(
   apiServer: Source[],
 ): Promise<Record<Source, Article>> {
-  const apiHost = 'https://nest.h7ml.cn/api/hotapi/';
-
-  const requests = apiServer.map((api) => axios.get(apiHost + api));
+  const requests = apiServer.map((api) => axios.get(apiPrefix + api));
   const responses = await Promise.all(requests);
 
   const results: Record<Source, Article> = {} as Record<Source, Article>;
@@ -127,73 +126,63 @@ async function generateReadme(data: any): Promise<string> {
   
   ## About Me ℹ️
   
-  [![前端物语](https://img.shields.io/badge/前端物语-4ABF8A?logo=Bloglovin&logoColor=fff)](https://www.h7ml.cn?q=github)
-  [![GitHub Stars](https://img.shields.io/github/stars/h7ml?color=2da44e&label=GitHub%20Stars&logo=Github)](https://github.com/h7ml)
-  [![visitors](https://visitor-badge.laobi.icu/badge?page_id=h7ml.h7ml)](https://github.com/h7ml)
-  [![CodeTime](https://img.shields.io/endpoint?style=social&url=https%3A%2F%2Fapi.codetime.dev%2Fshield%3Fid%3D3645%26project%3D%26in%3D0)](https://codetime.dev/zh-CN)
+  [![前端物语](https://nakoruru.h7ml.cn/proxy/img.shields.io/badge/前端物语-4ABF8A?logo=Bloglovin&logoColor=fff)](https://www.h7ml.cn?q=github)
+  [![GitHub Stars](https://nakoruru.h7ml.cn/proxy/img.shields.io/github/stars/h7ml?color=2da44e&label=GitHub%20Stars&logo=Github)](https://github.com/h7ml)
+  [![visitors](https://nakoruru.h7ml.cn/proxy/visitor-badge.laobi.icu/badge?page_id=h7ml.h7ml)](https://github.com/h7ml)
+  [![CodeTime](https://nakoruru.h7ml.cn/proxy/img.shields.io/endpoint?style=social&url=https%3A%2F%2Fapi.codetime.dev%2Fshield%3Fid%3D3645%26project%3D%26in%3D0)](https://codetime.dev/zh-CN)
   
   ## Languages 🌐
   
-  ![HTML5](https://img.shields.io/badge/HTML5-E34F26?logo=HTML5&logoColor=fff)
-  ![CSS3](https://img.shields.io/badge/CSS3-1572B6?logo=CSS3&logoColor=fff)
-  ![JavaScript](https://img.shields.io/badge/JavaScript-F7DF1E?logo=JavaScript&logoColor=333)
-  ![TypeScript](https://img.shields.io/badge/TypeScript-3178C6?logo=TypeScript&logoColor=fff)
+  ![HTML5](https://nakoruru.h7ml.cn/proxy/img.shields.io/badge/HTML5-E34F26?logo=HTML5&logoColor=fff)
+  ![CSS3](https://nakoruru.h7ml.cn/proxy/img.shields.io/badge/CSS3-1572B6?logo=CSS3&logoColor=fff)
+  ![JavaScript](https://nakoruru.h7ml.cn/proxy/img.shields.io/badge/JavaScript-F7DF1E?logo=JavaScript&logoColor=333)
+  ![TypeScript](https://nakoruru.h7ml.cn/proxy/img.shields.io/badge/TypeScript-3178C6?logo=TypeScript&logoColor=fff)
   
   ## Frameworks and Tools: 🛠️
   
-  ![React](https://img.shields.io/badge/React-61DAFB?logo=React&logoColor=333)
-  ![Next.js](https://img.shields.io/badge/Next.js-000000?logo=Next.js&logoColor=fff)
-  ![Vue.js](https://img.shields.io/badge/Vue.js-4FC08D?logo=Vue.js&logoColor=fff)
-  ![Sass](https://img.shields.io/badge/Sass-CC6699?logo=Sass&logoColor=fff)
-  ![Tailwind CSS](https://img.shields.io/badge/Tailwind%20CSS-06B6D4?logo=TailwindCSS&logoColor=fff)
-  ![Git](https://img.shields.io/badge/Git-F05032?logo=Git&logoColor=fff)
-  ![Visual Studio Code](https://img.shields.io/badge/VS%20CODE-007ACC?logo=VisualStudioCode&logoColor=fff)
+  ![React](https://nakoruru.h7ml.cn/proxy/img.shields.io/badge/React-61DAFB?logo=React&logoColor=333)
+  ![Next.js](https://nakoruru.h7ml.cn/proxy/img.shields.io/badge/Next.js-000000?logo=Next.js&logoColor=fff)
+  ![Vue.js](https://nakoruru.h7ml.cn/proxy/img.shields.io/badge/Vue.js-4FC08D?logo=Vue.js&logoColor=fff)
+  ![Sass](https://nakoruru.h7ml.cn/proxy/img.shields.io/badge/Sass-CC6699?logo=Sass&logoColor=fff)
+  ![Tailwind CSS](https://nakoruru.h7ml.cn/proxy/img.shields.io/badge/Tailwind%20CSS-06B6D4?logo=TailwindCSS&logoColor=fff)
+  ![Git](https://nakoruru.h7ml.cn/proxy/img.shields.io/badge/Git-F05032?logo=Git&logoColor=fff)
+  ![Visual Studio Code](https://nakoruru.h7ml.cn/proxy/img.shields.io/badge/VS%20CODE-007ACC?logo=VisualStudioCode&logoColor=fff)
   
   ## GitHub Stats 📊
   
-  ![h7ml's github stats](https://github-readme-stats.vercel.app/api?username=h7ml&show_icons=true&hide_title=true&count_private=true)
-  ![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=h7ml&layout=compact)
+  ![h7ml's github stats](https://nakoruru.h7ml.cn/proxy/github-readme-stats.vercel.app/api?username=h7ml&show_icons=true&hide_title=true&count_private=true)
+  ![Top Langs](https://nakoruru.h7ml.cn/proxy/github-readme-stats.vercel.app/api/top-langs/?username=h7ml&layout=compact)
   
   ## CodeTime: ⏱️
-  
-  <details>
-  <summary> Weekly development breakdown ⌛ </summary>
-  
-  ![wakatime](https://wakatime.com/share/@78c90c00-b60a-4b53-aca3-cdaada528717/e2a927a0-e579-4e6e-98cb-d769bbc3de2c.png)
 
-  </details>
-  
+  <img align='center' alt="wakatime" title="wakatime" src="https://nakoruru.h7ml.cn/proxy/wakatime.com/share/@78c90c00-b60a-4b53-aca3-cdaada528717/e2a927a0-e579-4e6e-98cb-d769bbc3de2c.png">
   `;
 
-  readme += '##  hotapi 🔥\n\n';
+  readme += '\n\n';
+  readme += '## [hotapi](https://nestjs.h7ml.cn/#/hotapi?q=github) 🔥\n\n';
   for (const source in data) {
     const articles = data[source];
-    readme += `${source}🔍\n\n`;
-    readme += `<details>\n\n`;
-    readme += `<summary> ${source} 🔍 </summary>\n\n`;
-    readme += `<ol>\n`;
-    articles.forEach((article) => {
-      readme += `<li>\n`;
-      readme += `[${article.title}](${article.url})\n`;
-      readme += `</li>\n`;
+    // readme += `${source}🔍\n`;
+    readme += `<details>\n`;
+    readme += `<summary> tag: ${source} 🔍 server: <a href="${apiPrefix}${source}?q=github" target="_blank">${apiPrefix}${source}</a>
+    </summary>\n`;
+    // readme += `<ol>\n`;
+    articles.forEach((article, index) => {
+      // readme += `<li>\n`;
+      readme += `\n ${index + 1} [${article.title}](${article.url})\n`;
+      // readme += `</li>\n`;
     });
-    readme += `</ol>\n`;
+    // readme += `</ol>\n`;
     readme += `</details>\n\n`;
     readme += `\n`;
   }
 
-  readme += `
-  <details>
+  readme += `## Metrics and Statistics 📈 
 
-  <summary>Metrics and Statistics 📈 </summary>
+  <img align='center' alt="Metrics" title="Metrics" src="https://nakoruru.h7ml.cn/proxy/metrics.lecoq.io/h7ml?template=classic&stars=1&people=1&repositories=1&starlists=1&achievements=1&notable=1&rss=1&steam=1&16personalities=1&base=header%2C%20activity%2C%20community%2C%20repositories%2C%20metadata&base.indepth=false&base.hireable=false&base.skip=false&repositories.batch=100&repositories.forks=false&repositories.affiliations=owner&stars=false&stars.limit=4&people=false&people.limit=24&people.identicons=true&people.identicons.hide=false&people.size=28&people.types=followers%2C%20following&people.shuffle=true&repositories=false&repositories.pinned=0&repositories.starred=0&repositories.random=0&repositories.order=featured%2C%20pinned%2C%20starred%2C%20random&starlists=false&starlists.limit=2&starlists.limit.repositories=2&starlists.languages=false&starlists.limit.languages=8&starlists.shuffle.repositories=true&achievements=false&achievements.threshold=C&achievements.secrets=true&achievements.display=detailed&achievements.limit=0&notable=false&notable.from=organization&notable.repositories=false&notable.indepth=false&notable.types=commit&notable.self=false&rss=false&rss.source=https%3A%2F%2Fwww.h7ml.cn%2Fsitemap.xml&rss.limit=4&steam=false&steam.sections=player%2C%20most-played%2C%20recently-played&steam.user=undefined&steam.games.limit=1&steam.recent.games.limit=1&steam.achievements.limit=2&steam.playtime.threshold=2&16personalities=false&16personalities.url=https%3A%2F%2Fwww.h7ml.cn&16personalities.sections=personality&16personalities.scores=true&config.timezone=Asia%2FShanghai&config.twemoji=true&config.octicon=true&config.display=columns">
 
-  ![Metrics](https://metrics.lecoq.io/h7ml?template=classic&base.repositories=0&isocalendar=1&languages=1&followup=1&people=1&code=1&notable=1&discussions=1&lines=1&repositories=1&introduction=1&gists=1&tweets=1&base.indepth=false&base.hireable=false&repositories=100&repositories.batch=100&repositories.forks=false&repositories.affiliations=owner&isocalendar.duration=half-year&languages.limit=8&languages.threshold=0%25&languages.other=false&languages.colors=github&languages.sections=most-used&languages.indepth=false&languages.analysis.timeout=15&languages.categories=markup%2C%20programming&languages.recent.categories=markup%2C%20programming&languages.recent.load=300&languages.recent.days=14&followup.sections=repositories&followup.indepth=false&followup.archived=true&people.limit=24&people.identicons=false&people.identicons.hide=false&people.size=28&people.types=followers%2C%20following&people.shuffle=false&code.lines=12&code.load=400&code.days=3&code.visibility=public&notable.from=organization&notable.repositories=false&notable.indepth=false&notable.types=commit&discussions.categories=true&discussions.categories.limit=0&repositories.pinned=0&introduction.title=true&tweets.user=.user.twitter&tweets.attachments=false&tweets.limit=2&config.timezone=Asia%2FShanghai)
-
-  </details>
-
-  ##### tThis Readme.md file is generated by [nestjs-nakoruru](https://github.com/h7ml/nestjs-nakoruru), with the domain [https://nestjs.h7ml.cn](https://nestjs.h7ml.cn) and authored by h7ml.
-
-    `;
+  ##### This Readme.md file is generated by [nestjs-nakoruru](https://github.com/h7ml/nestjs-nakoruru), with the domain [https://nestjs.h7ml.cn](https://nestjs.h7ml.cn) and authored by h7ml.
+  `;
 
   return readme;
 }
