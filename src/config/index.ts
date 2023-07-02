cfe5565fa57891bb6c649d1d2a2b214597af0ca0
h7ml <h7ml@qq.com>
2023-07-03 02:16:33 +0800

    feat: delete .env

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,9 +8,9 @@ export const TypeOrmConfig: TypeOrmModuleOptions = {
   type: 'mysql',
   host: process.env.DB_HOST || 'mysql.sqlpub.com',
   port: parseInt(process.env.DB_PORT || '3306'),
-  username: process.env.DB_USERNAME || 'xuziheng',
-  password: process.env.DB_PASSWORD || 'typeorm',
-  database: process.env.DB_DATABASE || 'xuziheng',
+  username: process.env.DB_USERNAME || '',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_DATABASE || '',
   retryDelay: parseInt(process.env.DB_RETRY_DELAY || '5000'),
   retryAttempts: parseInt(process.env.DB_RETRY_ATTEMPTS || '3'),
 };
